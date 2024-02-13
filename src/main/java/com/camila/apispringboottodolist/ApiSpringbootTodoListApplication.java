bc52b30aa79a554868afd202305cf067697eb4be
Camila <alvescamila87@gmail.com>
2024-02-13 20:02:53 -0300

    create project with springboot dependencies inicialized

---
+++ src/main/java/com/camila/apispringboottodolist/ApiSpringbootTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.camila.apispringboottodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringbootTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringbootTodoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/camila/apispringboottodolist/ApiSpringbootTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camila.apispringboottodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringbootTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
