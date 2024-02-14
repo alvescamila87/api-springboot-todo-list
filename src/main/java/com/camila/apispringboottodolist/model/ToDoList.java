4f72a7799551d1f7a07f1d5b5dc04276240e4fe6
Camila <alvescamila87@gmail.com>
2024-02-14 19:59:22 -0300

    create h2 application.properties, create model todolist and change controller

---
+++ src/main/java/com/camila/apispringboottodolist/model/ToDoList.java
@@ -0,0 +1,30 @@
+package com.camila.apispringboottodolist.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "todo_list")
+@Table(name = "todo_list")
+public class ToDoList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    private boolean done;
+
+    private int priority;
+}
