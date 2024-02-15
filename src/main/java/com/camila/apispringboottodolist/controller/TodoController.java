d27cb5ed40fcaf0bfe7e7bf1ca1dec03215db1c3
Camila <alvescamila87@gmail.com>
2024-02-15 17:37:25 -0300

    create entity, repository, service and controller

---
+++ src/main/java/com/camila/apispringboottodolist/controller/TodoController.java
@@ -0,0 +1,43 @@
+package com.camila.apispringboottodolist.controller;
+
+import com.camila.apispringboottodolist.entity.Todo;
+import com.camila.apispringboottodolist.service.TodoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.swing.plaf.PanelUI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    // service
+    private TodoService todoService;
+
+    // injeção via construtor
+    public TodoController(TodoService todoService) {
+        todoService = todoService;
+    }
+
+    @PostMapping
+    List<Todo> create(@RequestBody Todo todo) {
+        return todoService.create(todo);
+    }
+
+    @GetMapping
+    List<Todo> list() {
+        return todoService.list();
+    }
+
+    @PutMapping
+    List<Todo> update(@RequestBody Todo todo) {
+        return todoService.update(todo);
+    }
+
+    //DELETE /todos/1
+    @DeleteMapping("{id}")
+    List<Todo> delete(@PathVariable("id") Long id) {
+        return todoService.delete(id);
+    }
+}
---
+++ src/main/java/com/camila/apispringboottodolist/service/TodoService.java
@@ -0,0 +1,41 @@
+package com.camila.apispringboottodolist.service;
+
+import com.camila.apispringboottodolist.entity.Todo;
+import com.camila.apispringboottodolist.repository.TodoRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+
+    //reposiory
+    private TodoRepository todoRepository;
+
+    // injeção de dependência do componente de software
+    public TodoService(TodoRepository todoRepository) {
+        todoRepository = todoRepository;
+    }
+
+    public List<Todo> create(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list() {
+        Sort sort = Sort.by("propriedade").descending().and(Sort.by("nome").ascending());
+        return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> update(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> delete(Long id) {
+        todoRepository.deleteById(id);
+        return list();
+    }
+
+}
